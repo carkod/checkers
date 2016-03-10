4ab85264a9f8ad9820e5775612d50f8435af450a
carkod <disendaweb@gmail.com>
2016-03-10 17:23:46 +0100

    sync with Netbeans

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <div class="off-canvas-wrapper">
     <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
             
-		<?php get_template_part('responsive', 'nav')?>  
+	<?php get_template_part('responsive', 'nav')?>  
             
         <div class="off-canvas-content" data-off-canvas-content>
             
@@ -13,29 +13,27 @@
 			        
             <div id="primary" class="columns medium-7 float-right">
                 <main id="main" class="site-main" role="main">
-                <?php 
-				
-				switch($post_type) {
-				
-					case $post_type == ( is_post_type_archive('projects') || is_tax('job') ) :
-					get_template_part('loop', 'projects'); break;
-					
-					case $post_type == is_post_type_archive('resources')  :
-					get_template_part('loop', 'resources'); break;
-					
-					case $post_type == is_home() :
-					get_template_part('loop'); break;
-					
-					case $post_type == is_page() :
-					get_template_part('loop', 'page'); break;
-										
-				}
-				
-				?>
+                <?php switch($post_type) {
+
+                    case $post_type == ( is_post_type_archive('projects') || is_tax('job') ) :
+                    get_template_part('loop', 'projects'); break;
+
+                    case $post_type == is_post_type_archive('resources')  :
+                    get_template_part('loop', 'resources'); break;
+
+                    case $post_type == is_home() :
+                    get_template_part('loop'); break;
+
+                    case $post_type == is_page() :
+                    get_template_part('loop', 'page'); break;
+
+                } ?>
                 
                 </main><!-- .site-main -->
-               <div class="clearfix"></div>
-               <div id="footer" class="hide-for-print">
+                
+                <div class="clearfix"></div>
+               
+                <div id="footer" class="hide-for-print">
                     <footer>
                         <ul>
                             <li><?php _e('Inspired by Maria Pavlyuk','checkers')?>&nbsp;&middot;&nbsp;</li>
